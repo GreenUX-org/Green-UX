87fc56f976814773c7f26d58b82a3c4695a76884
Daniel Dip <dani.dip.2@gmail.com>
2023-01-25 13:35:52 -0300

    last pull

---
+++ api/src/models/Green.js
@@ -0,0 +1,20 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize)=>{
+    
+sequelize.define ('green', {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING
+      
+    }
+  }, {
+   
+  });
+} 
+
+
